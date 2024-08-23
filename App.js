637570d6ed1518dd0d7a7123932d000dc74381b0
kunallll17 <120559248+kunallll17@users.noreply.github.com>
2024-08-23 16:45:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './App.css';
+import SocialMediaCarousel from './SocialMediaCarousel'; // Import the Social Media component
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap
+
+function App() {
+  return (
+    <div className="App">
+      {/* Navigation Bar */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Features</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Pricing</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">About</a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+              <button className="btn btn-outline-primary" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </nav>
+
+      {/* Social Media Carousel */}
+      <SocialMediaCarousel />
+
+      {/* Other content or components can be added below */}
+    </div>
+  );
+}
+
+export default App;
